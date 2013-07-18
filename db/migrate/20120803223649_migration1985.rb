f8b3f70c02e056286b0d14df52b171354488f94e
Amanda Quaranto <amandaquaranto@ENG001065.local>
2013-07-17 17:47:15 -0700

    stupid syntax

--- db/migrate/20120803223649_migration1985.rb
+++ db/migrate/20120803223649_migration1985.rb
@@ -1,10 +1,10 @@
 class Migration1985 < ActiveRecord::Migration
   def up
-    add_column :name, :transactions, :string
+    add_column :transactions, :name, :string
     remove_column :cost
   end
 
   def down
-    remove_column :name, :transactions, :string
+    remove_column :transactions, :name, :string
   end
 end
